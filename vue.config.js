9c7320c0d68da4f5664ebeee1831364d4ea5d468
Sammie S. Taunton <diemuzi@gmail.com>
2022-03-05 11:25:01 -0600

    Sitemap Support

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,62 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+const paths = [
+    {
+        path: '/',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 1.0,
+        changefreq: 'hourly'
+    },
+    {
+        path: '/#/register',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.9,
+        changefreq: 'hourly'
+    },
+    {
+        path: '/#/schedule',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.8,
+        changefreq: 'hourly'
+    },
+    {
+        path: '/#/courses',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.7,
+        changefreq: 'daily'
+    },
+    {
+        path: '/#/faq',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.6,
+        changefreq: 'daily'
+    },
+    {
+        path: '/#/contact',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.5,
+        changefreq: 'monthly'
+    },
+    {
+        path: '/#/map',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.4,
+        changefreq: 'monthly'
+    },
+    {
+        path: '/#/coaches',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.3,
+        changefreq: 'monthly'
+    },
+    {
+        path: '/#/pictures',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: 0.2,
+        changefreq: 'weekly'
+    }
+];
+
 module.exports = {
     chainWebpack: config => {
         config
@@ -7,5 +66,10 @@ module.exports = {
 
                 return args;
             });
+    },
+    configureWebpack: {
+        plugins: [
+            new SitemapPlugin({ base: process.env.VUE_APP_GUI_URL, paths })
+        ]
     }
 }
