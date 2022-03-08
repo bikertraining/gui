7a1a5e0751194cdf3952f67a1410c79a7eccadd0
Sammie S. Taunton <diemuzi@gmail.com>
2022-03-08 14:08:00 -0600

    Removed HREF

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,10 +1,6 @@
 <template>
     <footer class="pt-3 mb-3 d-flex justify-content-between">
-        <span>
-            &copy; {{ new Date().getFullYear() }} <a class="text-decoration-none"
-                                                     href="https://bikertraining.net"
-                                                     target="_blank">Biker Training LLC</a>
-        </span>
+        &copy; {{ new Date().getFullYear() }} Biker Training LLC
     </footer>
 </template>
 
