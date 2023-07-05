1194665311896fb7dcbd5f4e8973444bd8fa9790
Sammie S. Taunton <diemuzi@gmail.com>
2023-07-05 10:21:47 -0500

    Cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
                     src: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js'
                 },
                 {
-                    src: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js',
+                    src: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js'
                 }
             ]
         }
