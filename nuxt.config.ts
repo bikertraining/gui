d717a6e241c5064c89e76e57e0222551d1a1d3d3
Sammie S. Taunton <diemuzi@gmail.com>
2023-09-01 12:07:53 -0500

    Sitemap exclusion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,6 +81,7 @@ export default defineNuxtConfig({
         credits: false,
         exclude: [
             '/admin/**',
+            '/callus',
             '/contact/confirmation',
             '/contact/unsubscribe/confirmation',
             '/payment/**',
