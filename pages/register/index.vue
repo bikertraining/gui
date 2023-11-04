c21bb9ed57e8bc0b502438af7e45e7b4b7dd5ec8
Sammie S. Taunton <diemuzi@gmail.com>
2023-11-04 11:22:25 -0500

    Fixed /register page, should redirect to /schedule

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
+const route = useRoute();
+
 const router = useRouter();
 
 onMounted(async () => {
     await router.push({ path: '/schedule' });
+});
+
+definePageMeta({
+    auth: false,
+    description: 'Register Online',
+    keywords: 'register online, register',
+    title: 'Register Online'
+});
+
+useHead({
+    title: `${route.meta['title']}`
 });
 </script>
 
