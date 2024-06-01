24fbf5c828850354fe215ba57713ac2b0e991a03
Sammie S. Taunton <diemuzi@gmail.com>
2024-06-01 14:40:18 -0500

    Updated BRP from 99 to 199

--- pages/courses/3wbrc.vue
+++ pages/courses/3wbrc.vue
@@ -122,17 +122,17 @@ useHead({
                     <NuxtLink class="mx-auto mt-3"
                               target="_blank"
                               to="https://learntoride3wheel.com/webreg/production/reactapp/?book=canamregister&SC=FLBKTR01&CC=3WBU">
-                        <img alt="Can-AM Learn to ride for $99"
+                        <img alt="Can-AM Learn to ride for $199"
                              class="d-block mx-auto img-fluid bg-black"
                              height="175"
                              loading="lazy"
-                             src="/img/brp/99.webp"
+                             src="/img/brp/199.webp"
                              width="400"/>
                     </NuxtLink>
 
                     <div class="mt-3 text-start mx-auto">
                         For a limited time, those that want to enroll in the 3-Wheel Basic RiderCourse on a Can-AM
-                        Spyder/Ryker can do so for only $99
+                        Spyder/Ryker can do so for only $199
                     </div>
                 </div>
 
